491e53a50959fb01ca182f9594f5dba81ea0586d
iyr <forwardsweep@gmail.com>
2021-01-14 11:34:22 -0700

    added password visibility obscuring, buttons to select db+key file paths

--- passwordManager.cpp
+++ passwordManager.cpp
@@ -13,6 +13,17 @@ char passwordField[FIELD_LENGTH] = {"The Quick Brown Fox Jumps Over The Lazy Dog
 char databaseField[FIELD_LENGTH] = {"some filepath idk"};
 char key_fileField[FIELD_LENGTH] = {"/path/to/file.key"};
 //     lol
+
+// Determines whether the filepath leads to a proper database file
+bool databaseValid(const char* filePath, stateMachine* sm){
+  return true;
+};
+
+// Any key file is valid
+bool key_fileValid(const char* filePath, stateMachine* sm){
+  return true;
+};
+
 FLASHMEM void doPassMan(stateMachine* sm){
 
   static uint8_t  currentSubScreen = 0;
@@ -25,22 +36,33 @@ FLASHMEM void doPassMan(stateMachine* sm){
   const   uint16_t  accentsColor  = sm->getDetailsColor();
   const   uint16_t  primaryColor  = sm->getPrimaryColor();
   const   uint16_t  averageColor  = sm->getAverageColor();
+  const   uint16_t  primaryColorI = sm->getPrimaryColorInverted();
 
   const   uint16_t  pressedKey    = sm->getPressedKey();
+
+  // Size of UI buttons
+  const   uint8_t   buttonRadX    = 27;
+  const   uint8_t   buttonRadY    = 21;
   
   //sm->tft->fillScreen(ILI9341_BLACK);
-  sm->tft->fillScreenHGradient(averageColor, primaryColor);
+  //sm->tft->fillScreenHGradient(averageColor, primaryColor);
+  sm->tft->fillScreenHGradient(primaryColor-1, averageColor);
   switch(currentSubScreen) {
 
     case PM_STATE_OPENDB:
-      static uint16_t passCursor = 0;
-      static uint16_t dbfpCursor = 0;
-      static uint16_t kyfpCursor = 0;
-      static bool     passActive = true;
-      static bool     dbfpActive = false;
-      static bool     kyfpActive = false;
+      static uint16_t passCursor  = 0;
+      static uint16_t dbfpCursor  = 0;
+      static uint16_t kyfpCursor  = 0;
+      static bool     passActive  = true;
+      static bool     dbfpActive  = false;
+      static bool     kyfpActive  = false;
+      static bool     hidePass    = true;
 
       sm->disableKeyStrokePassthrough();
+
+      //
+      //  Draw Password input field with visibility toggle button
+      //
       sm->tft->setFont(Arial_12);
       sm->tft->setTextDatum(TL_DATUM);
       sm->tft->setTextColor(accentsColor);
@@ -60,13 +82,17 @@ FLASHMEM void doPassMan(stateMachine* sm){
           sm->getReleasedKey()
           );
       // Draw text field
+      const uint8_t obscureBuffSize = 32;
+      char obscureBuff[obscureBuffSize] = {'\0'};
+      for (uint8_t i = 0; i < min(obscureBuffSize, strlen(passwordField)); i++) 
+        obscureBuff[i] = '*';
       if (drawTextInputField(
           offsetX, offsetY,
-          DISP_WIDTH-56, 15,
+          DISP_WIDTH-64, 15,
           LiberationMono_11,
           TL_DATUM,
-          passwordField,
-          FIELD_LENGTH,
+          hidePass?&obscureBuff:passwordField,
+          hidePass?min(obscureBuffSize, strlen(passwordField)+1):FIELD_LENGTH,
           &passCursor,
           passActive,
           sm
@@ -75,7 +101,22 @@ FLASHMEM void doPassMan(stateMachine* sm){
         dbfpActive = false;
         kyfpActive = false;
       }
+      // Draw Show/Hide password toggle
+      if (doIconButton(
+            DISP_WIDTH-buttonRadX-1, offsetY,
+            buttonRadX, buttonRadY, 0,
+            AwesomeF000_20,
+            (char)112,  // Crossed eye; obscure visibility
+            primaryColor,
+            hidePass?accentsColor:primaryColorI,
+            hidePass,
+            sm)){
+        hidePass = !hidePass;
+      }
 
+      //
+      //  Draw database filepath field with file open button
+      //
       offsetY += 32;
       sm->tft->setFont(Arial_12);
       sm->tft->setTextDatum(TL_DATUM);
@@ -99,7 +140,7 @@ FLASHMEM void doPassMan(stateMachine* sm){
       // Draw text field
       if (drawTextInputField(
           offsetX, offsetY,
-          DISP_WIDTH-56, 15,
+          DISP_WIDTH-64, 15,
           LiberationMono_11,
           TL_DATUM,
           databaseField,
@@ -113,6 +154,40 @@ FLASHMEM void doPassMan(stateMachine* sm){
         kyfpActive = false;
       }
 
+      char        Title[20]   = "Select Database";
+      uint8_t     TitleLen    = 20;
+      static bool selectingDB = false;
+      static bool DBselected  = false;
+      // Draw file open button
+      if (doIconButton(
+            DISP_WIDTH-buttonRadX-1, offsetY,
+            buttonRadX, buttonRadY, 0,
+            AwesomeF000_20,
+            (char)124,  // Crossed eye; obscure visibility
+            primaryColor,
+            accentsColor,
+            sm)){
+        selectingDB = true;
+        passActive  = false;
+        dbfpActive  = true;
+        kyfpActive  = false;
+      }
+      if (selectingDB)
+        DBselected = doFileOpen(
+          (char*)Title, 
+          TitleLen, 
+          databaseField,
+          databaseValid,
+          sm
+          );
+      if (DBselected){
+        selectingDB = false;
+        DBselected  = false;
+      }
+
+      //
+      //  Draw keyfile filepath field with file open button
+      //
       offsetY += 32;
       sm->tft->setFont(Arial_12);
       sm->tft->setTextDatum(TL_DATUM);
@@ -137,7 +212,7 @@ FLASHMEM void doPassMan(stateMachine* sm){
       // Draw text field
       if (drawTextInputField(
           offsetX, offsetY,
-          DISP_WIDTH-56, 15,
+          DISP_WIDTH-64, 15,
           LiberationMono_11,
           TL_DATUM,
           key_fileField,
@@ -149,6 +224,35 @@ FLASHMEM void doPassMan(stateMachine* sm){
         passActive = false;
         dbfpActive = false;
         kyfpActive = true;
+      }
+
+      static bool selectingKF = false;
+      static bool KFselected  = false;
+      // Draw file open button
+      if (doIconButton(
+            DISP_WIDTH-buttonRadX-1, offsetY,
+            buttonRadX, buttonRadY, 0,
+            AwesomeF000_20,
+            (char)124,  // Crossed eye; obscure visibility
+            primaryColor,
+            accentsColor,
+            sm)){
+        selectingKF = true;
+        passActive  = false;
+        dbfpActive  = true;
+        kyfpActive  = false;
+      }
+      if (selectingKF)
+        KFselected = doFileOpen(
+          (char*)Title, 
+          TitleLen, 
+          key_fileField,
+          key_fileValid,
+          sm
+          );
+      if (KFselected){
+        selectingKF = false;
+        KFselected  = false;
       }
       break;
 
@@ -174,3 +278,4 @@ FLASHMEM void doPassMan(stateMachine* sm){
 
   return;
 };
+
