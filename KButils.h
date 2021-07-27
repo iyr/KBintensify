8dbe754ca44fa83d2622768077c139c234f7e0d7
forwardsweep <forwardsweep@pop-os.localdomain>
2021-07-27 17:45:25 -0600

    added library include for output selector

--- KButils.h
+++ KButils.h
@@ -3,6 +3,7 @@
 #define SCREEN_HOME 1         // Home screen
 #define SCREEN_IMAGEVIEWER 2  //
 #define SCREEN_PASSWORDMAN 3  // Password Manager
+#define SCREEN_OUTPUTMUX 4    // Output multiplexor
 
 //#define SCREEN_CROSSHAIRDEMO 1 // stuff to do after calibration finished
 #include "keyLookUp.h"              // OEM key lookup table (US) and converter
@@ -10,9 +11,10 @@
 //#include "buttons.h"                // Utilites for drawing/watching buttons
 
 // KB-Functions
-#include "calibrationUtils.h"       // set screen calibration variables
-#include "homeScreen.h"             // Provide basic home screen
-#include "imageViewer.h"            // Basic image viewer to demonstrate file open dialogue
-#include "passwordManager.h"        // Provides encrypted password manager
+#include "calibrationUtils.h" // set screen calibration variables
+#include "homeScreen.h"       // Provide basic home screen
+#include "imageViewer.h"      // Basic image viewer
+#include "passwordManager.h"  // Provides encrypted password manager
+#include "outputmux.h"        // Select outputs for keystroke passthrough
 
 #include "dialogue.h"
