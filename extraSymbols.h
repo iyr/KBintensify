3983d0cee8a55fac2aafbbd6320a522d64b55528
forwardsweep <forwardsweep@pop-os.localdomain>
2021-08-06 20:23:05 -0600

    adds bluetooth and usb glyphs for text

---
+++ extraSymbols.h
@@ -0,0 +1,18 @@
+#ifndef _KB_extraSymbols_
+#define _KB_extraSymbols_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const uint8_t bt_16[];
+extern const uint8_t btxmit_16[];
+extern const uint8_t usb_16[];
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
