831769895adb43c6e1b655a2cd30f8fbfc612318
iyr <forwardsweep@gmail.com>
2021-11-20 17:07:19 -0700

    add instruction dialogue, update SD file objects, minor formatting

--- imageViewer.cpp
+++ imageViewer.cpp
@@ -5,14 +5,14 @@ void loadBmp(const char *filename, uint16_t *imgBuffer, stateMachine* sm);
 // These read 16- and 32-bit types from the SD card file.
 // BMP data is stored little-endian, Arduino is little-endian too.
 // May need to reverse subscript order if porting elsewhere.
-uint16_t read16(File &f) {
+uint16_t read16(FsFile &f) {
   uint16_t result;
   ((uint8_t *)&result)[0] = f.read(); // LSB
   ((uint8_t *)&result)[1] = f.read(); // MSB
   return result;
 }
 
-uint32_t read32(File &f) {
+uint32_t read32(FsFile &f) {
   uint32_t result;
   ((uint8_t *)&result)[0] = f.read(); // LSB
   ((uint8_t *)&result)[1] = f.read();
@@ -23,7 +23,7 @@ uint32_t read32(File &f) {
 
 // Determines whether the filepath leads to a proper bmp file
 bool imageValid(const char* filePath, stateMachine* sm){
-  File bmpFile;
+  FsFile bmpFile;
   uint8_t bmpDepth;
 
   if (!(bmpFile = sm->sd->open(filePath)))
@@ -44,41 +44,94 @@ bool imageValid(const char* filePath, stateMachine* sm){
 }
 
 void doImageViewer(stateMachine* sm){
-  const char      Title[14]     = "Select Image";
-  uint8_t         TitleLen      = 14;
-  static bool     selectionMade = false;
-  static bool     imageSelected = false;
+  char            Title[]           = "Selecting an Image";
+  uint8_t         TitleLen          = 18;
+  const char      helpMessage[]     = "Use the file browser to select an image file (.bmp supported only). After an image has been opened, tap the screen to select different file.";
+  const uint16_t  helpMessageLen    = 140;
+  const char*     options[]         = {"Ok"};
+  uint8_t         optionLengths[]   = {3};
+  ILI9341_t3_font_t optionFonts[]   = {Arial_18};
+  /*
+  const char      helpMessage[]     = "Use the file browser to select an image file (.bmp supported only). After an image has been opened, tap the screen to select another one. The quick brown fox jumps over the lazy dog. Voluptas quia dolorum velit hic illo ut. Animi impedit quis temporibus eaque voluptas molestiae. Id est eum rerum officia ipsum unde. Tempora praesentium veniam animi nostrum labore nisi quia voluptatem. Et id ex delectus culpa totam deleniti. Deleniti consequuntur et eligendi.";
+  const uint16_t  helpMessageLen    = 461;
+  const char*     options[]         = {"Ok", "Direc\nHeck", "Top\nQuack", "I\nUnderstand\n(proceed)"};
+  uint8_t         optionLengths[]   = {3, 3, 3, 3};
+  ILI9341_t3_font_t optionFonts[]   = {Arial_18, Arial_12, Arial_12, Arial_8};
+  */
+  static bool     selectionMade     = false;
+  static bool     imageSelected     = false;
+  static uint8_t  helpMessageAck    = 0;
+
+	const		bool			screenWasPressed			= sm->screenWasPressed();
+	const		bool			screenWasLongPressed	= sm->screenWasLongPressed();
+	const		bool			screenWasReleased			= sm->screenWasReleased();
   
   // Convenience placeholders
-  const uint16_t  accentsColor  = sm->getDetailsColor();
-  const uint16_t  primaryColor  = sm->getPrimaryColor();
-  const uint16_t  TouchX        = sm->getTouchX();
-  const uint16_t  TouchY        = sm->getTouchY();
-  sm->tft->fillScreen(ILI9341_BLACK);
+  const uint16_t  accentsColor      = sm->getDetailsColor();
+  const uint16_t  primaryColor      = sm->getPrimaryColor();
+  const uint16_t  averageColor      = sm->getAverageColor();
+  const uint16_t  accentsColorI     = sm->getDetailsColorInverted();
+  const uint16_t  primaryColorI     = sm->getPrimaryColorInverted();
+  const uint16_t  TouchX            = sm->getTouchX();
+  const uint16_t  TouchY            = sm->getTouchY();
+  const uint16_t  optionColors[]    = {accentsColor, accentsColor, accentsColor, accentsColor};
+
+  // Inform user how to display an image
+  if (helpMessageAck == 0) {
+    helpMessageAck = doMultiOptMessage(
+        (char)90,      // i in a circle ~ information symbol
+        AwesomeF000_12,
+        (char *)Title,
+        TitleLen,
+        helpMessage,
+        helpMessageLen,
+        1,
+        options,
+        optionLengths,
+        optionColors,
+        optionFonts,
+        sm
+        );
+    if (//helpMessageAck == 1 ||
+        helpMessageAck == 2 ||
+        helpMessageAck == 3 || 
+        helpMessageAck == 4){
+			Serial.print("Button Pressed: ");
+			Serial.println(helpMessageAck);
+      helpMessageAck = 0;
+    }
+    //if (helpMessageAck) helpMessageAck = 0;
+    return;
+  }
 
   if (selectionMade) {
     // User canceled file-open, return to home screen
     if (strlen((char *)&sm->filePathBuff) == 0) {
       selectionMade = false;
+      imageSelected = false;
       //sm->setScreen(SCREEN_HOME);
       sm->setScreen(1);
     } 
     // User selected a file path to image
     else {
       if (!imageSelected) { // only load image once
-        loadBmp((char *)&sm->filePathBuff, wallpaper, sm);
+        //loadBmp((char *)&sm->filePathBuff, wallpaper, sm);
+        loadBmp((char *)&sm->filePathBuff, sm->userBackground, sm);
+        
         imageSelected = true;
       } else {  // draw loaded image
-        sm->tft->writeRect(0, 0, DISP_WIDTH, DISP_HEIGHT, wallpaper);
+        sm->tft->writeRect(0, 0, DISP_WIDTH, DISP_HEIGHT, sm->userBackground);
       }
 
       // Exit on touch
-      if (!sm->getCurrTouch() && sm->getPrevTouch()) {
+      if (screenWasPressed) {
         selectionMade = false;
         imageSelected = false;
       }
     }
   } else {
+    memset(Title, '\0', 18);
+    strcpy("Select Image File:", Title);
     selectionMade = doFileOpen(
         (char*)Title, 
         TitleLen, 
@@ -94,6 +147,18 @@ void doImageViewer(stateMachine* sm){
     //sm->tft->drawFastVLine(TouchX, 0, (uint16_t)DISP_HEIGHT, accentsColor);
   //}
 
+  // reset vars if user exits/returns to home screen via home button
+  if (  screenWasPressed                    &&
+        //TouchX <= 2*buttonRadX              &&
+        TouchX <= 2*28                      &&
+        TouchX >= 0                         &&
+        //TouchY <= DISP_HEIGHT-2*buttonRadY  &&
+        TouchY <= DISP_HEIGHT-2*24          &&
+        TouchY >= DISP_HEIGHT               ){
+    selectionMade     = false;
+    imageSelected     = false;
+  }
+
   return;
 }
 
@@ -101,18 +166,18 @@ void doImageViewer(stateMachine* sm){
 #define BUFFPIXEL 80
 void loadBmp(const char *filename, uint16_t *imgBuffer, stateMachine* sm){
   uint16_t  x=0,y=0;
-  File     bmpFile;
-  int      bmpWidth, bmpHeight;   // W+H in pixels
-  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
-  uint32_t bmpImageoffset;        // Start of image data in file
-  uint32_t rowSize;               // Not always = bmpWidth; may have padding
-  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
-  uint16_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
-  boolean  goodBmp = false;       // Set to true on valid header parse
-  boolean  flip    = true;        // BMP is stored bottom-to-top
-  int      w, h, row, col;
-  uint8_t  r, g, b;
-  uint32_t pos = 0, startTime = millis();
+  FsFile    bmpFile;
+  int       bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t   bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t  bmpImageoffset;        // Start of image data in file
+  uint32_t  rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t   sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+  uint16_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean   goodBmp = false;       // Set to true on valid header parse
+  boolean   flip    = true;        // BMP is stored bottom-to-top
+  int       w, h, row, col;
+  uint8_t   r, g, b;
+  uint32_t  pos = 0, startTime = millis();
 
   if((x >= DISP_WIDTH) || (y >= DISP_HEIGHT)) return;
 
