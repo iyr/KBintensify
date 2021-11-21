1730f52f3edad270194b251ef91fab6739038a1e
iyr <forwardsweep@gmail.com>
2021-11-20 17:00:36 -0700

    updated includes

--- buttons.h
+++ buttons.h
@@ -10,15 +10,10 @@
 #include <XPT2046_Touchscreen.h>
 #endif
 
-#include <SdFat.h>
-#include <SdFatConfig.h>
-#include <sdios.h>
-
 #include <TimeLib.h>  // Provide Real-time clock
 #include "smStruct.h"
 
 // Draws a button with a glyph symbol and text
-// Constant Size, not scalable
 // Returns True if the button has been pressed
 bool doIconTextButton(
       uint16_t          posX,       // X-Position to draw button
@@ -27,6 +22,7 @@ bool doIconTextButton(
       const uint8_t     radY,       // Y-radius of the button
       const int8_t      Yadjust,    // used for fine-tuning troublesome glyphs
       char*             iconTitle,  // null-term'd Title text of shortcut
+			const uint8_t			titleLen,		// length, in chars, of title text
       ILI9341_t3_font_t symbolFont, // Font for shortcut symbol glyph
       char              symbol,     // shortcut symbol glyph
       const uint16_t    bgColor,    // Button background color
@@ -80,4 +76,18 @@ bool doIconButton(
       const uint16_t    bgColor,    // Button background color
       const uint16_t    dtColor,    // Button details/accents color
       stateMachine*     sm          // Statemachine
+      );
+
+// Draws a button with just text
+// Returns True if the button has been pressed
+bool doTextButton(
+      uint16_t          posX,     // X-Position to draw button
+      uint16_t          posY,     // Y- Position to draw button
+      const uint8_t     radX,     // X-radius of the button
+      const uint8_t     radY,     // Y-radius of the button
+      char*             text,			// null-term'd text of button 
+      ILI9341_t3_font_t textFont,	// Font for button text
+      const uint16_t    bgColor,  // Button background color
+      const uint16_t    dtColor,  // Button details/accents color
+      stateMachine*     sm        // Statemachine
       );
